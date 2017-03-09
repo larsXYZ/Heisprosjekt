b890ae84b71d5c93fccfec3f625ca157217dbd24
chrismur <chrismur@stud.ntnu.no>
2017-03-09 19:01:29 +0100

    Add files via upload

--- timer.c
+++ timer.c
@@ -1,21 +1,56 @@
 #include "timer.h"
-
+#include <stdio.h>
 
 void timehandler_init(struct Timehandler *timehandler)
 {
 	timehandler->timeout = -1;
 }
 
-void timehandler_delay(struct Timehandler *timehandler, int delay)
-{
-	time_t current_time;
-	current_time = time(NULL);
-	timehandler->timeout = current_time + delay;
+void timehandler_delay(struct Timehandler *timehandler, double delay)
+{	 
+	clock_t t;
+	t = clock();
+	timehandler->timeout = t + CLOCKS_PER_SEC*delay;
 
 }
 
 int timehandler_is_time_up(struct Timehandler *timehandler)
 {
 	if (timehandler->timeout == -1) return -1;
-	else return (time(NULL) >= timehandler->timeout);
+	else return (clock() >= timehandler->timeout);
+}
+
+int main() {
+	struct Timehandler timehandler;
+
+	system("PAUSE");
+	timehandler_delay(&timehandler, 3);
+	while (1) {
+		if (timehandler_is_time_up(&timehandler)) {
+			break;
+		}
+	}
+	printf("OK");
+	timehandler_delay(&timehandler, 3);
+	while (1) {
+		if (timehandler_is_time_up(&timehandler)) {
+			break;
+		}
+	}
+	printf("OK");
+	timehandler_delay(&timehandler, 3);
+	while (1) {
+		if (timehandler_is_time_up(&timehandler)) {
+			break;
+		}
+	}
+	printf("OK");
+	timehandler_delay(&timehandler, 3);
+	while (1) {
+		if (timehandler_is_time_up(&timehandler)) {
+			break;
+		}
+	}
+	printf("OK");
+	return 0;
 }
