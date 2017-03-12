a3b482379a3efcfc7b6777a9280c0fa949209725
chrismur <chrismur@stud.ntnu.no>
2017-03-12 15:48:36 +0100

    Update timehandler.h

--- timehandler.h
+++ timehandler.h
@@ -1,12 +1,14 @@
+//Handles time criterias using processor clock
+
 #include <time.h>
 struct Timehandler
 {
 	int timeout;
 };
 
-void timehandler_init(struct Timehandler *target);
+void timehandler_init(struct Timehandler *target); //Initializes the timehandler object
 
-void timehandler_delay(struct Timehandler *target, int delay);
+void timehandler_delay(struct Timehandler *target, int delay); //Sets a delay time
 
-int timehandler_is_time_up(struct Timehandler *target);
+int timehandler_is_time_up(struct Timehandler *target); //Checks if delay time is reached
 
